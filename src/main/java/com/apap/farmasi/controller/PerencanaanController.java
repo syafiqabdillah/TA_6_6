551735f5a8f34296751f8646ecdd0b307346c9a9
syafiqabdillah <abdillah.syafiq@gmail.com>
2018-11-22 06:01:34 +0700

    minor changes di PerencanaanController dan PermintaanModel

--- src/main/java/com/apap/farmasi/controller/PerencanaanController.java
+++ src/main/java/com/apap/farmasi/controller/PerencanaanController.java
@@ -3,8 +3,11 @@ package com.apap.farmasi.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.client.RestTemplate;
 
 import com.apap.farmasi.model.PerencanaanModel;
 import com.apap.farmasi.service.PerencanaanService;
@@ -21,6 +24,5 @@ public class PerencanaanController {
 		model.addAttribute("perencanaan", perencanaan);
 		return "view-perencanaan-pembelian-medical-supplies";
 	}
-	
 	
 }
--- src/main/java/com/apap/farmasi/model/PermintaanModel.java
+++ src/main/java/com/apap/farmasi/model/PermintaanModel.java
@@ -13,7 +13,6 @@ import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Size;
 
 import org.hibernate.annotations.OnDelete;
 import org.hibernate.annotations.OnDeleteAction;
