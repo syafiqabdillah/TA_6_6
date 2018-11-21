68ab4289799e11ba6ab91216e6201e21c79e8fdd
syafiqabdillah <abdillah.syafiq@gmail.com>
2018-11-21 17:33:45 +0700

    fitur 10

---
+++ src/main/java/com/apap/farmasi/controller/PerencanaanController.java
@@ -0,0 +1,26 @@
+package com.apap.farmasi.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.apap.farmasi.model.PerencanaanModel;
+import com.apap.farmasi.service.PerencanaanService;
+
+
+@Controller
+public class PerencanaanController {
+	@Autowired 
+	PerencanaanService perencanaanService;
+	
+	@RequestMapping(value = "/medical-supplies/perencanaan", method = RequestMethod.GET)
+	private String view(Model model) {
+		PerencanaanModel perencanaan = perencanaanService.getPerencanaan();
+		model.addAttribute("perencanaan", perencanaan);
+		return "view-perencanaan-pembelian-medical-supplies";
+	}
+	
+	
+}
