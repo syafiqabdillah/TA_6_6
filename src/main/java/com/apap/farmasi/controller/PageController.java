baccff2cb3fec8bd00856d895f5e7921cdb02d53
Irawan Firmansyah <firmansyah_irawan@yahoo.co.id>
2018-12-08 23:18:43 +0700

    update home,login,webconfig,and fragment

--- src/main/java/com/apap/farmasi/controller/PageController.java
+++ src/main/java/com/apap/farmasi/controller/PageController.java
@@ -1,8 +1,13 @@
 package com.apap.farmasi.controller;
 
+import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,16 +21,47 @@ import com.apap.farmasi.service.MedicalSuppliesService;
 public class PageController {
 	@Autowired
 	MedicalSuppliesService medsupService;
-	
+
 	@RequestMapping(value ="/", method = RequestMethod.GET)
 	public String home(Model model) {
-		List<MedicalSuppliesModel> listMedicalSupplies = medsupService.getAll();
+		List<MedicalSuppliesModel> listMedicalSupplies = new ArrayList<>();
 		PerencanaanModel perencanaan = new PerencanaanModel();
+		java.sql.Date date = new java.sql.Date(Calendar.getInstance().getTime().getTime());
+		perencanaan.setTanggal(date);
+		if (minggu1Atau3()) {
+			System.out.println("ini minggu 1 atau 3");
+		}
+		if (minggu1Atau3()) {
+			//kalo minggu 1 atau 3, bebas 
+			listMedicalSupplies = medsupService.getAll();
+		} else {
+			//kalo gak minggu 1 atau 3, cuma yang urgent 
+			List<MedicalSuppliesModel> allMedsup = medsupService.getAll();
+			for (MedicalSuppliesModel medsup : allMedsup) {
+				//cuma masukin yang urgent 
+				if (medsup.getJenisMedicalSupplies().getUrgent().getId() == 1) {
+					listMedicalSupplies.add(medsup);
+				} else {
+					System.out.println(medsup.getNama() + " tidak dimasukkan ke pilihan karena tidak urgent ");
+				}
+			}
+		}
+		Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+		String authority = authentication.getAuthorities().iterator().next().getAuthority();
+		
+		model.addAttribute("authority", authority);
 		model.addAttribute("perencanaan", perencanaan);
 		model.addAttribute("listMedicalSupplies", listMedicalSupplies);
 		return "home";
 	}
-	
+
+	boolean minggu1Atau3() {
+		Calendar calendar = Calendar.getInstance();
+		int minggu = calendar.get(Calendar.WEEK_OF_MONTH);
+		System.out.println("ini minggu ke-" + minggu);
+		return (minggu==1) || (minggu==3);
+	}
+
 	@RequestMapping("/login")
 	public String login() {
 		return "login";
