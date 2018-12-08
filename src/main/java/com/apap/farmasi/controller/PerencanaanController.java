8e16da3038df0dc35c3ffc1703347e21e0c18105
syafiqabdillah <abdillah.syafiq@gmail.com>
2018-12-08 15:51:49 +0700

    minor changes

--- src/main/java/com/apap/farmasi/controller/PerencanaanController.java
+++ src/main/java/com/apap/farmasi/controller/PerencanaanController.java
@@ -1,6 +1,9 @@
 package com.apap.farmasi.controller;
 
+import java.io.IOException;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -9,10 +12,16 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.client.RestTemplate;
 
+import com.apap.farmasi.model.MedicalSuppliesModel;
 import com.apap.farmasi.model.PerencanaanModel;
+import com.apap.farmasi.model.StaffModel;
 import com.apap.farmasi.service.MedicalSuppliesService;
 import com.apap.farmasi.service.PerencanaanService;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 @Controller
 public class PerencanaanController {
@@ -57,6 +66,25 @@ public class PerencanaanController {
 		List<PerencanaanModel> listPerencanaan = perencanaanService.getAll();
 		model.addAttribute("listPerencanaan", listPerencanaan);
 		return "view-perencanaan-pembelian-medical-supplies";
+	}
+	
+	private <List>MedicalSuppliesModel getDataFromLab() throws IOException{
+		//rest template 
+		RestTemplate restTemplate = new RestTemplate();
+		//membuat Object mapper 
+    	ObjectMapper mapper = new ObjectMapper();
+    	//path
+    	String path = "https://44d5b7c7-1ad6-43ca-8645-2673bcad019b.mock.pstmn.io/lab/kebutuhan/perencanaan";
+    	//json map 
+    	String jsonLab = restTemplate.getForObject(path, String.class);
+    	JsonNode jsonNodeLab = mapper.readTree(jsonLab);
+    	//mengambil result , yakni map
+    	String resultMap = jsonNodeLab.get("result").toString();
+    	System.out.println("result map : " + resultMap);
+    	//merubah string of map of Json menjad map of MedsupLab
+    	Map<String, StaffModel> mapStaff = mapper.readValue(resultMap, new TypeReference<HashMap<String, StaffModel>>(){});
+    	
+		return null;
 	}
 	
 }
