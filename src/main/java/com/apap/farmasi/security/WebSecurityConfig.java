1fd229bd3948c4dc6a71b0a60d57413d0c93b46b
Irawan Firmansyah <firmansyah_irawan@yahoo.co.id>
2018-12-09 13:18:50 +0700

    adding sign up form, updating login form, webconfig

--- src/main/java/com/apap/farmasi/security/WebSecurityConfig.java
+++ src/main/java/com/apap/farmasi/security/WebSecurityConfig.java
@@ -33,6 +33,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 			.antMatchers("/rawat-jalan/obat/tambah").hasAnyAuthority("ADMIN_FARMASI","STAF_APOTEKER")
 			.antMatchers("/medical-supplies/updateStatus").hasAnyAuthority("ADMIN_FARMASI")
 			.antMatchers("/api/**").permitAll()
+			.antMatchers("/user/**").permitAll()
 			.anyRequest().authenticated()
 			.and()
 			.formLogin()
