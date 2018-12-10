07e0df1455d0e162d099ab2199a8d7e90b2a41e8
syafiqabdillah <abdillah.syafiq@gmail.com>
2018-12-10 09:50:30 +0700

    bug di update status perencanaan fixed

--- src/main/java/com/apap/farmasi/controller/PerencanaanController.java
+++ src/main/java/com/apap/farmasi/controller/PerencanaanController.java
@@ -6,6 +6,8 @@ import java.util.List;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -34,6 +36,9 @@ public class PerencanaanController {
 	@RequestMapping(value = "/medical-supplies/perencanaan", method = RequestMethod.GET)
 	private String view(Model model) {
 		List<PerencanaanModel> listPerencanaan = perencanaanService.getAll();
+		Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+		String authority = authentication.getAuthorities().iterator().next().getAuthority();
+		model.addAttribute("authority", authority);
 		model.addAttribute("listPerencanaan", listPerencanaan);
 		return "view-perencanaan-pembelian-medical-supplies";
 	}
