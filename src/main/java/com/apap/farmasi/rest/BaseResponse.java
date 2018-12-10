2da3522c892a766d42ef77ceafecf390aaebb2b9
Irawan Firmansyah <firmansyah_irawan@yahoo.co.id>
2018-12-10 10:57:26 +0700

    adding BaseResponse, update login page, home page

---
+++ src/main/java/com/apap/farmasi/rest/BaseResponse.java
@@ -0,0 +1,32 @@
+package com.apap.farmasi.rest;
+
+public class BaseResponse<T> {
+	
+	public int status;
+	private String message;
+	private T result;
+	
+	public int getStatus() {
+		return status;
+	}
+	
+	public void setStatus(int status) {
+		this.status = status;
+	}
+	
+	public String getMessage() {
+		return message;
+	}
+	
+	public void setMessage(String message) {
+		this.message = message;
+	}
+	
+	public T getResult() {
+		return result;
+	}
+	
+	public void setResult(T result) {
+		this.result = result;
+	}
+}
